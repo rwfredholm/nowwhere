f72d5b1f8e9d06c86bb06a6e88e91b97fdcc9597
Rasmus Fredholm <rwfredholm@gmail.com>
2013-05-13 18:02:32 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nowwhere::Application.config.secret_token = 'f110d141068e1870f928f20a314e93e45bef114d2430a5f0571678a12e75e0eb69519a8b0b2e2e83b9780e65a7b44696cd24fe010a05e46575ace055493fe023'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nowwhere::Application.config.session_store :cookie_store, key: '_nowwhere_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nowwhere::Application.config.session_store :active_record_store
